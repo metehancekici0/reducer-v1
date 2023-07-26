c838c9b3bc2b76648fce1694d4abb6c7aacef59a
Metehan Çekici <metehancekici0@gmail.com>
2023-07-26 14:38:55 +0300

    React Reducer ile ilk proje

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import { useReducer } from 'react';
+import { reducer } from "./reducer";
+
+const initialState = {
+  data: "",
+  loading: false,
+  error: ""
+}
 
 function App() {
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const { data, loading, error } = state;
+
+  const fetchDog = () => {
+
+    dispatch({ type: "FETCH_START" });
+
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((res) => res.json())
+      .then((res) => {
+        dispatch({ type: "FETCH_SUCCESS", payload: res.message });
+      })
+      .catch(() => {
+        dispatch({ type: "FETCH_ERROR", payload: "Error fetching data" });
+      })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={fetchDog} disabled={loading} >Fetch Dog İmage</button>
+      {data && (
+        <div>
+          <img src={data} alt="Random Dog" />
+        </div>
+      )}
+      {error && <p> {error} </p>}
+    </>
   );
 }
 
